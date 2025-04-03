296bee761668af354c1cab278bd93fa05b712b74
JulianC775 <juliancatello4@gmail.com>
2025-04-02 23:33:55 -0700

    Refactor Tic Tac Toe game layout and functionality. Updated index.html to remove placeholder text in cells. Enhanced script.js to implement game logic, including turn switching and win detection. Added styling for game cells in styles.css to improve user interaction.

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -3,4 +3,44 @@ const playerTwo = '0'
 
 let whoseTurn = playerOne
 
+const cells = document.querySelectorAll('#cell-0, #cell-1, #cell-2, #cell-3, #cell-4, #cell-5, #cell-6, #cell-7, #cell-8')
+
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        cell.textContent = whoseTurn
+
+        winningCombinations.forEach(combination => {
+            const [a, b, c] = combination
+            console.log('Gello')
+            if (cells[a].textContent === cells[b].textContent && cells[b].textContent === cells[c].textContent && cells[a].textContent !== '') {
+                alert(`${whoseTurn} wins!`)
+                cells.forEach(cell => {
+                    cell.textContent = ''
+                })
+            }
+        })  
+
+        if (whoseTurn === playerOne) {
+            whoseTurn = playerTwo
+        } else {
+            whoseTurn = playerOne
+        }
+
+    
+    })
+})  
+
+
 
