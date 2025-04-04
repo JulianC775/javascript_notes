b47f5ad89848360034d8ffb35f62dbea95c7af29
JulianC775 <julianc775@gmail.com>
2025-04-04 01:47:25 -0700

    Add click event listeners to interactive elements in Memory Game
    Enhanced script.js by adding event listeners for the test button and div, allowing them to log descriptive messages when clicked. This improves user interaction and debugging capabilities.

--- memory_game/script.js
+++ memory_game/script.js
@@ -1,10 +1,14 @@
+// Get references to DOM elements
 const button = document.getElementById("test-button");
 const div = document.getElementById("test-div");
 
+// Handler function that logs which element was clicked
 const handleThingClicked = (thing) => {
     console.log(thing)
 }
 
+// Add click event listeners to both elements
+// When clicked, they will call handleThingClicked with a descriptive message
 button.addEventListener("click", ()=>handleThingClicked('button clicked'));
 div.addEventListener("click", ()=>handleThingClicked('div clicked'));
 
